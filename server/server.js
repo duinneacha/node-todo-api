d092ac284d850ffb3aa1e080626228fbe72b1020
Aidan Dennehy <duinneacha@gmail.com>
2018-09-02 21:29:38 +0100

    Added GET

--- server/server.js
+++ server/server.js
@@ -10,6 +10,8 @@ const app = express();
 // Middleware confiruration - bodyParser will control the format of the json
 app.use(bodyParser.json());
 
+
+// Post Route
 app.post('/todos', (req, res) => {
   console.log(req.body);
   var todo = new Todo({
@@ -22,6 +24,17 @@ app.post('/todos', (req, res) => {
     res.status(400).send(error);
   });
 });
+
+// Get Route
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({ todos });
+  }, (error) => {
+    res.status(400).send(error);
+  });
+});
+
+
 
 app.listen(3000, () => {
   console.log('Started on Port 3000');
