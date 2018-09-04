ffd9fe94aff2f5f42beba3f6c2ef8c5827d5734c
Aidan Dennehy <duinneacha@gmail.com>
2018-09-04 16:25:27 +0100

    Added Environment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,4 +1,7 @@
 // Mongoose
+
+require('./../config/config');
+
 const mongoose = require('mongoose');
 
 // The following line sets the mongoose promise set to the default promise set
@@ -8,12 +11,25 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-// Connect to a database
-// mongoose.connect('mongodb://127.0.0.1:27017/TodoApp', { useNewUrlParser: true });
+// Connect to a database!!!!!! USE 127.0.0.1 line for localhost and mlab for Heroku
 
-mongoose.connect('mongodb://127.0.0.1:27017/TodoApp', { useNewUrlParser: true });
+//mongoose.connect('mongodb://127.0.0.1:27017/TodoApp', { useNewUrlParser: true });
 
-// mongoose.connect('mongodb://admin:Passw0rd@ds243812.mlab.com:43812/adtodos', { useNewUrlParser: true });
+var env = process.env.NODE_ENV || 'development';
+
+if (env === 'production') {
+  mongoose.connect('mongodb://admin:Passw0rd@ds243812.mlab.com:43812/adtodos', { useNewUrlParser: true });
+} else if (env === 'development') {
+  process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoApp';
+  console.log('Connecting to MongoDB locally!');
+  mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+} else {
+  process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
+  mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+}
+
+
+
 
 // Create a model - so that mongoose knows how to store our data - the attributes we want it to have
 // (Mongo can store schema less data in documents - mongoose likes a model)
